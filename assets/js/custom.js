62af7a7303a53d13d720c4ef6e13546e1561e0ca
Kazeeem <35298707+Kazeeem@users.noreply.github.com>
2019-04-04 16:47:28 +0100

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,48 @@
+﻿(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+                      
+            //HOVER DROPDOWN SCRIPTS
+            $('ul.nav li.dropdown').hover(function () {
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+            }, function () {
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+            });
+
+            //TESTIMONIAL & SLIDEER SPEED
+            $('.carousel').carousel({
+                interval: 4000 //TIME IN MILLI SECONDS
+            })
+            // PRETTYPHOTO FUNCTION 
+
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+           
+            /*====================================
+            WRITE YOUR SCRIPTS HERE
+            ======================================*/
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
+
+
+var aboutText = document.getElementById("aboutText");
+function showAboutText() {
+    aboutText.style.display = 'block';
+    document.getElementById("button").style.display = 'none';
+}
